9fec90261fdb34d2decaa05097df3e6d67805f3c
Joana Sousa <jsousa039@gmail.com>
2020-06-11 17:56:42 +0200

    Created new unit tests to check if the requirements are met

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,97 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void nameFooTest() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    public void qualityDecreaseTest(){
+        Item[] itemToTest = new Item[] {new Item("item", 5, 11)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(10, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void qualityDecreaseTwiceTest(){
+        Item[] itemToTest = new Item[]{new Item("test", -1, 10)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(8,gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void qualityIsNotNegativeTest(){
+        Item[] itemToTest = new Item[]{new Item("test", 10, 0)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertTrue(gildedRose.items[0].quality >= 0);
+    }
+
+    @Test
+    public void qualityIsNotMoreThan50(){
+        Item[] itemToTest = new Item[]{new Item("Aged Brie", 5, 50)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(50, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void sellInDecreaseTest(){
+        Item[] itemToTest = new Item[] {new Item("test", 11, 10)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(10, gildedRose.items[0].sellIn);
+    }
+
+    @Test
+    public void agedBrieQualityIncreaseTest(){
+        Item[] itemToTest = new Item[] {new Item("Aged Brie", 6, 9)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(10, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void sulfurasNotChangeSellInAndQuality(){
+        Item[] itemToTest = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 10, 10)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(10, gildedRose.items[0].sellIn);
+        assertEquals(10, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void backstagePass10DaysQualityIncrease(){
+        Item[] itemToTest = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(12, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void backstagePass5DaysQualityIncrease(){
+        Item[] itemToTest = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 12)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(15, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void backstagePass0DaysQualityDecrease(){
+        Item[] itemToTest = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 15)};
+        GildedRose gildedRose = new GildedRose(itemToTest);
+        gildedRose.updateQuality();
+        assertEquals(0, gildedRose.items[0].quality);
     }
 
 }
