20cb7b1cfdbc4e189aa71f1239e7a973a3e21d26
Joana Sousa <jsousa039@gmail.com>
2020-06-12 11:26:16 +0200

    Refactored the updateQuality method.
    Created multiple private method to make the code more readable and easier to understand.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,54 +9,103 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+            Item item = items[i];
+            if (!isAgedBrieItem(item) && !isBackstagePassItem(item)) {
+                if (isPositiveQualityItem(item)) {
+                    if (!isSulfurasItem(item)) {
+                        decreaseQuality(item);
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (isQualityBelowFifty(item)) {
+                    increaseQuality(item);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (isBackstagePassItem(item)) {
+                        if (isSellInDaysTenOrBelow(item)) {
+                            if (isQualityBelowFifty(item)) {
+                                increaseQuality(item);
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (isSellInDaysFiveOrBelow(item)) {
+                            if (isQualityBelowFifty(item)) {
+                                increaseQuality(item);
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!isSulfurasItem(item)) {
+                decreaseSellIn(item);
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (hasSellInDaysFinished(item)) {
+                if (!isAgedBrieItem(item)) {
+                    if (!isBackstagePassItem(item)) {
+                        if (isPositiveQualityItem(item)) {
+                            if (!isSulfurasItem(item)) {
+                                decreaseQuality(item);
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        decreaseQualityToZero(item);
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (isQualityBelowFifty(item)) {
+                        increaseQuality(item);
                     }
                 }
             }
         }
+    }
+
+    private boolean isAgedBrieItem(Item item){
+        return item.name.equalsIgnoreCase("Aged Brie");
+    }
+
+    private boolean isSulfurasItem(Item item){
+        return item.name.equalsIgnoreCase("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isBackstagePassItem(Item item){
+        return item.name.equalsIgnoreCase("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isPositiveQualityItem(Item item){
+        return item.quality > 0;
+    }
+
+    private void decreaseQuality(Item item){
+        item.quality = item.quality - 1;
+    }
+
+    private void increaseQuality(Item item){
+        item.quality = item.quality + 1;
+    }
+
+    private void decreaseSellIn(Item item){
+        item.sellIn = item.sellIn - 1;
+    }
+
+
+    private boolean isQualityBelowFifty(Item item){
+        return item.quality < 50;
+    }
+
+    private boolean isSellInDaysTenOrBelow(Item item){
+        return item.sellIn <= 10;
+    }
+
+    private boolean isSellInDaysFiveOrBelow(Item item){
+        return item.sellIn <= 5;
+    }
+
+    private boolean hasSellInDaysFinished(Item item){
+        return item.sellIn < 0;
+    }
+
+    private void decreaseQualityToZero(Item item){
+        item.quality = 0;
     }
 }
