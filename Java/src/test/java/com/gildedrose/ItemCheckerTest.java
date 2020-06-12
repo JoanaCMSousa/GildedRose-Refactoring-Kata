894e4441b0281d6ffae9bdc0902837c3b74da9a8
Joana Sousa <jsousa039@gmail.com>
2020-06-12 15:31:21 +0200

    Created a new test class to test the method of ItemChecker class

--- Java/src/test/java/com/gildedrose/ItemCheckerTest.java
+++ Java/src/test/java/com/gildedrose/ItemCheckerTest.java
@@ -1,4 +1,32 @@
 package com.gildedrose;
 
+import com.gildedrose.helpers.ItemChecker;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ItemCheckerTest {
+
+    @Test
+    public void isAgedBrieTest(){
+        Item item = new Item("Aged Brie", 10, 10);
+        assertTrue(ItemChecker.isAgedBrieItem(item));
+    }
+
+    @Test
+    public void isLegendaryItemTest(){
+        Item item = new Item("Sulfuras, Hand of Ragnaros", 10, 10);
+        assertTrue(ItemChecker.isLegendaryItem(item));
+    }
+
+    @Test
+    public void isBackstagePassItemTest(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10);
+        assertTrue(ItemChecker.isBackstagePassItem(item));
+    }
+
+    @Test
+    public void isConjuredItemTest(){
+        Item item = new Item("Conjured Mana Cake", 10, 10);
+        assertTrue(ItemChecker.isConjuredItem(item));
+    }
 }
