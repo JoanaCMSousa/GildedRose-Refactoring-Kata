68000f4b2b0aa3391ae68c07c9432f223f106e3f
Joana Sousa <jsousa039@gmail.com>
2020-06-14 10:46:56 +0200

    Refactored class ItemChecker. IT has now private final static variables.

--- Java/src/main/java/com/gildedrose/helpers/ItemChecker.java
+++ Java/src/main/java/com/gildedrose/helpers/ItemChecker.java
@@ -4,20 +4,25 @@ import com.gildedrose.Item;
 
 public class ItemChecker {
 
+    private final static String AGED_BRIE_ITEM = "Aged Brie";
+    private final static String LEGENDARY_ITEM = "Sulfuras, Hand of Ragnaros";
+    private final static String BACKSTAGE_PASS_ITEM = "Backstage passes to a TAFKAL80ETC concert";
+    private final static String CONJURED_ITEM = "conjured";
+
     public static boolean isAgedBrieItem(Item item){
-        return item.name.equalsIgnoreCase("Aged Brie");
+        return item.name.equalsIgnoreCase(AGED_BRIE_ITEM);
     }
 
     public static boolean isLegendaryItem(Item item){
-        return item.name.equalsIgnoreCase("Sulfuras, Hand of Ragnaros");
+        return item.name.equalsIgnoreCase(LEGENDARY_ITEM);
     }
 
     public static boolean isBackstagePassItem(Item item){
-        return item.name.equalsIgnoreCase("Backstage passes to a TAFKAL80ETC concert");
+        return item.name.equalsIgnoreCase(BACKSTAGE_PASS_ITEM);
     }
 
     public static boolean isConjuredItem(Item item){
-        return item.name.toLowerCase().contains("conjured");
+        return item.name.toLowerCase().contains(CONJURED_ITEM);
     }
 
     public static boolean isQualityBelowFifty(Item item){
