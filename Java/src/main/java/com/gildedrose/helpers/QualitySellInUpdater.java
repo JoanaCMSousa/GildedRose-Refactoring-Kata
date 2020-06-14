b2cb2fe54f62b43d880599c05c93489a3813f4ab
Joana Sousa <jsousa039@gmail.com>
2020-06-14 10:25:40 +0200

    Fixed the methods increaseQuality and decreaseQuality so the values don't surpass the limits

--- Java/src/main/java/com/gildedrose/helpers/QualitySellInUpdater.java
+++ Java/src/main/java/com/gildedrose/helpers/QualitySellInUpdater.java
@@ -5,11 +5,21 @@ import com.gildedrose.Item;
 public class QualitySellInUpdater {
 
     public static void decreaseQuality(Item item, int decreaseDownTo){
-        item.quality = item.quality - decreaseDownTo;
+        if(item.quality - decreaseDownTo <= 0){
+            item.quality = 0;
+        }
+        else {
+            item.quality = item.quality - decreaseDownTo;
+        }
     }
 
     public static void increaseQuality(Item item, int sumUpTo){
-        item.quality = item.quality + sumUpTo;
+        if((item.quality + sumUpTo) >= 50){
+            item.quality = 50;
+        }
+        else {
+            item.quality = item.quality + sumUpTo;
+        }
     }
 
     public static void decreaseSellIn(Item item){
