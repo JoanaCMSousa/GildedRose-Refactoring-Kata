57481da414fa3f07d23a5c48ed8b131e7d9a3dd5
Joana Sousa <jsousa039@gmail.com>
2020-06-14 16:28:05 +0200

    Created a new class called QualityValues which contains the constants relevant for QualityUpdater and QualityChecker

--- Java/src/main/java/com/gildedrose/helpers/qualityvalues/QualityValues.java
+++ Java/src/main/java/com/gildedrose/helpers/qualityvalues/QualityValues.java
@@ -1,4 +1,6 @@
 package com.gildedrose.helpers.qualityvalues;
 
-public class QualityValues {
+public final class QualityValues {
+    public final static int QUALITY_MAX = 50;
+    public final static int QUALITY_MIN = 0;
 }
