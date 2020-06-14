2ab54d63e728e9fdcc9be6fc2dc36fd7a0530297
Joana Sousa <jsousa039@gmail.com>
2020-06-14 16:25:45 +0200

    Refactored ItemUpdater accordingly witht he refactor of QualityUpdater

--- Java/src/main/java/com/gildedrose/helpers/ItemUpdater.java
+++ Java/src/main/java/com/gildedrose/helpers/ItemUpdater.java
@@ -8,24 +8,24 @@ public class ItemUpdater {
 
     public static void updateAgedBrieQuality(Item item){
         if (ItemChecker.hasSellInDaysFinished(item)) {
-            QualitySellInUpdater.increaseQuality(item, 2);
+            QualityUpdater.increaseQuality(item, 2);
         } else {
-            QualitySellInUpdater.increaseQuality(item, 1);
+            QualityUpdater.increaseQuality(item, 1);
         }
 
     }
 
     public static void updateBackstagePassQuality(Item item){
         if(ItemChecker.hasSellInDaysFinished(item) ){
-            QualitySellInUpdater.setQualityToMin(item);
+            QualityUpdater.setQualityToMin(item);
         }
         else {
             if (ItemChecker.isSellInDaysFiveOrBelow(item)) {
-                QualitySellInUpdater.increaseQuality(item, 3);
+                QualityUpdater.increaseQuality(item, 3);
             } else if (ItemChecker.isSellInDaysTenOrBelow(item)) {
-                QualitySellInUpdater.increaseQuality(item, 2);
+                QualityUpdater.increaseQuality(item, 2);
             } else {
-                QualitySellInUpdater.increaseQuality(item, 1);
+                QualityUpdater.increaseQuality(item, 1);
             }
             }
 
@@ -33,18 +33,18 @@ public class ItemUpdater {
 
     public static void updateConjuredItem(Item item){
         if(ItemChecker.hasSellInDaysFinished(item)){
-            QualitySellInUpdater.decreaseQuality(item, 4);
+            QualityUpdater.decreaseQuality(item, 4);
         } else {
-            QualitySellInUpdater.decreaseQuality(item, 2);
+            QualityUpdater.decreaseQuality(item, 2);
         }
 
     }
 
     public static void updateNormalItem(Item item){
         if (ItemChecker.hasSellInDaysFinished(item)) {
-            QualitySellInUpdater.decreaseQuality(item, 2);
+            QualityUpdater.decreaseQuality(item, 2);
         } else {
-            QualitySellInUpdater.decreaseQuality(item, 1);
+            QualityUpdater.decreaseQuality(item, 1);
         }
 
     }
