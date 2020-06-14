1a432d31e5d5ed8503f53735efd3c2dda4e6b9b4
Joana Sousa <jsousa039@gmail.com>
2020-06-14 11:38:46 +0200

    Added more contannts to ItemChecker.
    Also created a private constructor.

--- Java/src/main/java/com/gildedrose/helpers/ItemChecker.java
+++ Java/src/main/java/com/gildedrose/helpers/ItemChecker.java
@@ -8,6 +8,10 @@ public class ItemChecker {
     private final static String LEGENDARY_ITEM = "Sulfuras, Hand of Ragnaros";
     private final static String BACKSTAGE_PASS_ITEM = "Backstage passes to a TAFKAL80ETC concert";
     private final static String CONJURED_ITEM = "conjured";
+    private final static int QUALITY_MAX = 50;
+    private final static int QUALITY_MIN = 0;
+
+    private ItemChecker(){}
 
     public static boolean isAgedBrieItem(Item item){
         return item.name.equalsIgnoreCase(AGED_BRIE_ITEM);
@@ -25,8 +29,8 @@ public class ItemChecker {
         return item.name.toLowerCase().contains(CONJURED_ITEM);
     }
 
-    public static boolean isQualityBelowFifty(Item item){
-        return item.quality < 50;
+    public static boolean isQualityBelowMax(Item item){
+        return item.quality < QUALITY_MAX;
     }
 
     public static boolean isSellInDaysTenOrBelow(Item item){
@@ -41,7 +45,7 @@ public class ItemChecker {
         return item.sellIn < 0;
     }
 
-    public static boolean isQualityZero(Item item){
-        return item.quality == 0;
+    public static boolean isQualityMinOrBelow(Item item){
+        return item.quality <= QUALITY_MIN;
     }
 }
