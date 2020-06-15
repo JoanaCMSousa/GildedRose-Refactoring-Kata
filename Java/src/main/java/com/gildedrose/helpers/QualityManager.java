74ca8c62b16fadb3f109a4874e2c2c37ce0355c4
Joana Sousa <jsousa039@gmail.com>
2020-06-15 19:08:16 +0200

    Renamed the method isQualityMinOrBelow to isQualityMinOrABove in QualityManager class
    Changed the constants from public to private
    
    Changed the methods isQualityMaxOrBelow and isQualityMinOrAbove to public since they are now used in GildedRose
    
    Refactored the class QualityManager

--- Java/src/main/java/com/gildedrose/helpers/QualityManager.java
+++ Java/src/main/java/com/gildedrose/helpers/QualityManager.java
@@ -4,36 +4,29 @@ import com.gildedrose.Item;
 
 public class QualityManager {
 
-    public final static int QUALITY_MAX = 50;
-    public final static int QUALITY_MIN = 0;
+    private final static int QUALITY_MAX = 50;
+    private final static int QUALITY_MIN = 0;
 
     private QualityManager(){}
 
     static void decreaseQuality(Item item, int decreaseBy){
-        item.quality = item.quality - decreaseBy;
-
-        if(isQualityMinOrBelow(item)){
-            item.quality = QUALITY_MIN;
-        }
+        item.quality = Math.max(item.quality - decreaseBy, QUALITY_MIN);
     }
 
     static void increaseQuality(Item item, int increaseBy){
-        item.quality = item.quality + increaseBy;
+        item.quality = Math.min(item.quality + increaseBy, QUALITY_MAX);
 
-        if(!isQualityMaxOrBelow(item)){
-            item.quality = QUALITY_MAX;
-        }
     }
 
     static void setQualityToMin(Item item){
-        item.quality = 0;
+        item.quality = QUALITY_MIN;
     }
 
-    static boolean isQualityMaxOrBelow(Item item){
+    public static boolean isQualityMaxOrBelow(Item item){
         return item.quality <= QUALITY_MAX;
     }
 
-    static boolean isQualityMinOrBelow(Item item){
-        return item.quality <= QUALITY_MIN;
+    public static boolean isQualityMinOrAbove(Item item){
+        return item.quality >= QUALITY_MIN;
     }
 }
